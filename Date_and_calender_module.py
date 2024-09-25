3504f2529d659e2911aa22a7c4824b7085c1ef9f
aayan776 <forcodingproffesion@gmail.com>
2024-09-25 17:04:54 +0600

    Date_and_calender_module.py

--- Date_and_calender_module.py
+++ Date_and_calender_module.py
@@ -24,3 +24,27 @@ print(f"Leap days in the 21st century: {leap_years}")
 specific_date = datetime.date(9999, 12, 31)
 weekday = specific_date.strftime("%A")
 print(f"{specific_date} is a {weekday}")
+try:
+    def planecost(city):
+        if city == "dhaka":
+            return 4000
+        elif city == "khulna":
+            return 5500
+        elif city == "mymensingh":
+            return 3000
+    def hotelcost(nights):
+        return nights * 1000
+    def carcost(days):
+        return days * 200
+    travel_city = input("Enter city you are travelling to: ").lower()
+    nights_spent = int(input("Enter amount of nights spent: "))
+    days_travelled = int(input("Enter amount of days to rent a car for: "))
+    city_travelled = planecost(travel_city)
+    hotel_cost = hotelcost(nights_spent)
+    car_cost = carcost(days_travelled)
+    result = city_travelled + hotel_cost + car_cost
+    print(f"Total expenses: {result}")
+    str = str(result)
+    print(len(str))
+except ValueError:
+    print("Value error detected")
